cc7469dfcda55cc92f1947537343862cc345bede
Mauyr Alexandre Pereira <mauyr.pereira@cvccorp.com.br>
2018-11-27 11:44:50 -0200

    Testes para extração de nome do grupo e projeto

---
+++ tests/utilities/test_git_helper.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from utilities.git_helper import GitHelper
+
+
+class TestGitHelper(TestCase):
+    def test_extract_group_and_project_from_url_ssh(self):
+        url = "git@git.domain.com.br:group/project.git"
+        group, project = GitHelper.extract_group_and_project_from_url(url)
+        self.assertEqual(group, 'group')
+        self.assertEqual(project, 'project')
+
+    def test_extract_group_and_project_from_url_http(self):
+        url = "https://git.domain.com.br/group/project.git"
+        group, project = GitHelper.extract_group_and_project_from_url(url)
+        self.assertEqual(group, 'group')
+        self.assertEqual(project, 'project')
