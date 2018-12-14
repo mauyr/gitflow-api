9335b9a41ffa9347b628f7385f7309d2ffbc974c
Mauyr Alexandre Pereira <mauyr.pereira@cvccorp.com.br>
2018-12-14 16:33:15 -0200

    Suporte a Angular 1 com npm

--- gitflow_api/project/angular_project.py
+++ gitflow_api/project/angular_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import json
 
 from gitflow_api.project.project_manager import ProjectManager
 
@@ -7,16 +8,21 @@ from gitflow_api.project.project_manager import ProjectManager
 class AngularProject(ProjectManager):
 
     def __init__(self, path):
-        super.__init__(path)
+        super().__init__(path)
 
     def update_dependencies_version(self):
         pass
 
     def actual_version(self):
-        return
+        with open(AngularProject._get_version_filename()) as f:
+            data = json.load(f)
+            return data["version"]
 
     def update_version(self, version):
-        return
+        with open(AngularProject._get_version_filename()) as f:
+            data = json.load(f)
+            data["version"] = version
+            return AngularProject._write_new_version(data, AngularProject._get_version_filename())
 
     def dependencies(self):
         return
@@ -26,3 +32,13 @@ class AngularProject(ProjectManager):
 
     def deploy(self):
         return
+
+    @staticmethod
+    def _get_version_filename():
+        return 'package.json'
+
+    @staticmethod
+    def _write_new_version(data, version_filename):
+        version_file = open(version_filename, 'w')
+        json.dump(data, version_file, indent=4)
+        version_file.truncate()
---
+++ tests/project/test_angular_project.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from unittest import TestCase
+from unittest.mock import patch
+
+import os
+
+from gitflow_api.project.angular_project import AngularProject
+
+
+class TestAngularProject(TestCase):
+
+    @staticmethod
+    def get_correct_package_file():
+        return 'mock/package.json.mock'
+
+    @patch.object(AngularProject, '_get_version_filename', get_correct_package_file)
+    def test_actual_version_with_correct_setup(self):
+        angular_project = AngularProject(os.path)
+        actual_version = angular_project.actual_version()
+        self.assertEqual(actual_version, '1.0.0')
+
+    @staticmethod
+    def write_fake_package(data, filename):
+        return data
+
+    @patch.object(AngularProject, '_get_version_filename', get_correct_package_file)
+    @patch.object(AngularProject, '_write_new_version', write_fake_package)
+    def test_update_version(self):
+        angular_project = AngularProject(os.path)
+        new_file = angular_project.update_version('2.0.0')
+
+        self.assertTrue(new_file["version"] == "2.0.0")
