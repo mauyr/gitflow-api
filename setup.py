64c30f7e3127d2fc8ca7e2d01935a6e5d97c15b8
Mauyr Alexandre Pereira <mauyr.pereira@cvccorp.com.br>
2019-01-23 16:38:54 -0200

    Update to next version 0.0.27

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requires = []
 
 setup(
     name="gitflow-api",
-    version="0.0.26",
+    version="0.0.27",
     description="Gitflow-API library using a API as backend",
     author="Mauyr Alexandre Pereira",
     author_email="mauyr.pereira@inovapro.com.br",
